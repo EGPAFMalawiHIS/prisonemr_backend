9a990ea8e10aa38d97d82d8fa471f7714cf69f3d
pamzey <mvumapatrick@gmail.com>
2024-10-31 11:37:02 +0200

    added migration file

--- db/migrate/20220105061145_add_result_push_status_to_lab_lims_order_mapping.rb
+++ db/migrate/20220105061145_add_result_push_status_to_lab_lims_order_mapping.rb
@@ -3,6 +3,9 @@
 # migration to add a column to the lim order mapping
 class AddResultPushStatusToLabLimsOrderMapping < ActiveRecord::Migration[5.2]
   def change
-    add_column :lab_lims_order_mappings, :result_push_status, :boolean, default: false
+    unless column_exists?(:lab_lims_order_mappings, :result_push_status)
+      add_column :lab_lims_order_mappings, :result_push_status, :boolean, default: false
+    end
   end
 end
+
